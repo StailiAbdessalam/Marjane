0d3e665c30a5779bb8c2fed3363c94d96916a50f
abdessalm staili <abdoycode@gmail.com>
2022-11-06 15:46:23 +0100

    docs : add promotion servlet in a database

--- src/main/java/Controllers/AdminCenter/ServletPromotionAccess.java
+++ src/main/java/Controllers/AdminCenter/ServletPromotionAccess.java
@@ -1,20 +1,35 @@
 package Controllers.AdminCenter;
 
+import Dao.AdminCenterDao;
+import Dao.CategorieDao;
+import Models.Categorie;
+import Models.Centreadmin;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
 
 @WebServlet(name = "ServletPromotionAccess", value = "/PromotionAccess")
 public class ServletPromotionAccess extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        CategorieDao categorieDao = new CategorieDao();
+        List allcategorie = categorieDao.getAllCategories();
+        request.setAttribute("Allcategorie", allcategorie);
+        request.getRequestDispatcher("./AdminCenter/Promotion.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int idcentrerAdmin = (int) request.getAttribute("idcenter");
+        int idCategorie = (int) request.getAttribute("idcateorie");
+        int porsantage = (int) request.getAttribute("postantage");
+        Instant datestart = (Instant) request.getAttribute("datestart");
+        Instant datefinish = (Instant) request.getAttribute("datefinish");
+        String description = (String) request.getAttribute("description");
 
     }
 }
