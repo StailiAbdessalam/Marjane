b5790a9556f63ee0482ce75f38f4a43112f3e3dd
abdessalm staili <abdoycode@gmail.com>
2022-11-11 09:55:33 +0100

    fix : update column of entity

--- src/main/java/Models/Promotion.java
+++ src/main/java/Models/Promotion.java
@@ -9,37 +9,48 @@ import java.time.Instant;
 
 @Entity
 @Table(name = "promotion")
+@Data
 @NoArgsConstructor
-public class Promotion implements Serializable{
+public class Promotion implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "p_id", nullable = false)
     private Integer id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "p_idcenteradmin", nullable = false)
-    private Centreadmin pIdcenteradmin;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "p_categorie", nullable = false)
-    private Categorie pCategorie;
+    @Column(name = "p_createdat", nullable = false)
+    private Instant pCreatedat;
 
     @Column(name = "p_point", nullable = false)
     private Integer pPoint;
 
     @Column(name = "p_status")
-    private Boolean pStatus;
+    private String pStatus;
 
-    @Column(name = "p_createdat", nullable = false)
-    private Instant pCreatedat;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "p_categorie", nullable = false)
+    private Categorie pCategorie;
 
-    public Promotion(Centreadmin pIdcenteradmin, Categorie pCategorie, Integer pPoint, Boolean pStatus, Instant pCreatedat) {
-        this.pIdcenteradmin = pIdcenteradmin;
-        this.pCategorie = pCategorie;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "p_idcenteradmin", nullable = false)
+    private Centreadmin pIdcenteradmin;
+
+    @Column(name = "date_debut", length = Integer.MAX_VALUE)
+    private String dateDebut;
+
+    @Column(name = "description", length = Integer.MAX_VALUE)
+    private String description;
+
+    @Column(name = "\"datExpire\"", length = Integer.MAX_VALUE)
+    private String datExpire;
+
+    public Promotion(Instant pCreatedat, Integer pPoint, String pStatus, Categorie pCategorie, Centreadmin pIdcenteradmin, String dateDebut, String description, String datExpire) {
+        this.pCreatedat = pCreatedat;
         this.pPoint = pPoint;
         this.pStatus = pStatus;
-        this.pCreatedat = pCreatedat;
+        this.pCategorie = pCategorie;
+        this.pIdcenteradmin = pIdcenteradmin;
+        this.dateDebut = dateDebut;
+        this.description = description;
+        this.datExpire = datExpire;
     }
-
-
 }
