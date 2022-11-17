1e87f58cc7c8a356a382d24cf04a741baa2528b1
abdessalm staili <abdoycode@gmail.com>
2022-11-17 11:13:32 +0100

    docs: ServletLogout
    ServletLogout to delete session

--- src/main/java/Controllers/ServletLogout.java
+++ src/main/java/Controllers/ServletLogout.java
@@ -3,18 +3,16 @@ package Controllers;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-
 import java.io.IOException;
 
 @WebServlet(name = "ServletLogout", value = "/logout")
 public class ServletLogout extends HttpServlet {
 
+    // delete session value
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if(session != null){
-            session.invalidate();
-        }
+        HttpSession session = request.getSession();
+        session.invalidate();
         request.getRequestDispatcher("/index.jsp").forward(request,response);
     }
 }
